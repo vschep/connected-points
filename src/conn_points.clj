6dca5b6b7b6aafad02b5e4469eaaada074fb6725
Viktor Schepik <viktor.schepik@beyondstruggle.de>
2022-01-06 18:39:36 +0100

    add color palette

--- src/conn_points.clj
+++ src/conn_points.clj
@@ -21,15 +21,26 @@
            (fn [x] (map (fn [y] [x y]) ys))
            xs))))
 
+(def color-index (atom -1))
+
+(defn next-color [palette cycle]
+  (swap! color-index
+         #(mod (inc %) cycle))
+  (nth palette @color-index))
+
 (c2d/with-canvas
   [c my-canvas]
-  (let [[_ _ point-color line-color] (color/palette 56)]
+  (let [point-color (color/color 210 210 210)
+        line-colors (color/palette 90)
+        cycle (count line-colors)]
     (c2d/set-background c 255 255 255)
     (doseq [[x1 y1] points
             [x2 y2] points]
-      (c2d/set-color c line-color)
-      (c2d/line c x1 y1 x2 y2))
-    (doseq [[x1 y1] points
+      (let [line-color (next-color line-colors cycle)]
+        (prn [x1 y1 x2 y2 line-color])
+        (c2d/set-color c line-color)
+        (c2d/line c x1 y1 x2 y2)))
+    #_(doseq [[x1 y1] points
             [x2 y2] points]
       (c2d/set-color c point-color)
       (c2d/ellipse c x1 y1 3 3))))
